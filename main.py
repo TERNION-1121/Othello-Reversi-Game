1ca924190068ee5d1e6c4e31140839c654051268
Vikrant Singh Bhadouriya <97667653+TERNION-1121@users.noreply.github.com>
2023-08-20 21:34:22 +0530

    Adding logic to add legal moves
    Logic to find Legal moves for a particular player added.
    Added a new image ring.png, this would be displayed on the positions (with low opacity) that are included under legal moves at a specific turn for a particular player.
    Extending main.py to display rings (partially completed).
    
    > Add logic to place discs with mouse

--- main.py
+++ main.py
@@ -15,12 +15,16 @@ bBoard = pygame.image.load("images/Othello_Black_Side_Board.png")
 wBoard = pygame.image.load("images/Othello_White_Side_Board.png")
 black_disc = pygame.image.load("images/Black_Disc.png")
 white_disc = pygame.image.load("images/White_Disc.png")
+ring = pygame.image.load("images/ring.png").convert()
+pygame.Surface.set_alpha(ring, 30)
 
 
 screen.blit(bBoard, (0,0))
 pygame.display.flip()
 
 running = True
+turn = Board.BLACK
+shown_moves = False
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -28,16 +32,24 @@ while running:
 
     for row in range(8):
         for col in range(8):
-            if game_board.board[row, col] == 1:
+            if game_board.board[row, col] == Board.BLACK:
                 x = 100 + 75 * col
                 y = 100 + 75 * row
                 screen.blit(black_disc, (x,y))
                 pygame.display.flip()
 
-            elif game_board.board[row, col] == -1:
+            elif game_board.board[row, col] == Board.WHITE:
                 x = 100 + 75 * col
                 y = 100 + 75 * row
                 screen.blit(white_disc, (x,y))
                 pygame.display.flip()
+    
+    if turn == Board.BLACK and not shown_moves:
+        possible_moves = list(game_board.all_legal_moves(Board.BLACK))
+        for pos in possible_moves:
+            r, c = pos
+            screen.blit(ring, (100 + 75 * c, 100 + 75 * r))
+            pygame.display.flip()
+        shown_moves = True
 
 pygame.quit()
