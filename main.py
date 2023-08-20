daebf85ac152f077c29674566ba2f1b168f2465f
Vikrant Singh Bhadouriya <97667653+TERNION-1121@users.noreply.github.com>
2023-08-20 16:33:38 +0530

    Updating images
    Image over image bug is with linux, not in Windows.
    Done with new backgrounds and discs.
    
    > Game Logic Next

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pygame
 from functions import *
 
@@ -10,7 +9,8 @@ pygame.init()
 
 SCREEN_SIZE = 800
 screen = pygame.display.set_mode((SCREEN_SIZE, SCREEN_SIZE))
-screen.fill((255,255,255))
+pygame.display.set_caption("Othello/Reversi")
+
 bBoard = pygame.image.load("images/Othello_Black_Side_Board.png")
 wBoard = pygame.image.load("images/Othello_White_Side_Board.png")
 black_disc = pygame.image.load("images/Black_Disc.png")
@@ -29,15 +29,15 @@ while running:
     for row in range(8):
         for col in range(8):
             if game_board.board[row, col] == 1:
-                x = 100 * col + 5
-                y = 100 * row + 5
-                # screen.blit(black_disc, (x,y))
-                # pygame.display.flip()
+                x = 100 + 75 * col
+                y = 100 + 75 * row
+                screen.blit(black_disc, (x,y))
+                pygame.display.flip()
 
             elif game_board.board[row, col] == -1:
-                x = 100 * col + 5
-                y = 100 * row + 5
-                # screen.blit(white_disc, (x,y))
-                # pygame.display.flip()
+                x = 100 + 75 * col
+                y = 100 + 75 * row
+                screen.blit(white_disc, (x,y))
+                pygame.display.flip()
 
 pygame.quit()
