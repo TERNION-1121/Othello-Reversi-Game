dc512f587614fcfbe65c27a6c9ae39f048123cfe
Vikrant Singh Bhadouriya <97667653+TERNION-1121@users.noreply.github.com>
2023-09-28 15:37:38 +0530

    alpha beta pruning

--- src/utils/minimax.py
+++ src/utils/minimax.py
@@ -1,6 +1,6 @@
 from utils.board import Board
 
-def minimax(position: Board, depth: int, isMaximizingPlayer: bool) -> int:
+def minimax(position: Board, depth: int, alpha: int, beta: int, isMaximizingPlayer: bool) -> int:
     if depth == 0 or position.check_game_over() is True:
         return position.evaluate_board()
     
@@ -12,11 +12,15 @@ def minimax(position: Board, depth: int, isMaximizingPlayer: bool) -> int:
                 position.board[row, col] = Board.BLACK
 
                 opponents_moves = position.all_legal_moves(Board.WHITE)
-                eval = minimax(position, depth - 1, len(opponents_moves) == 0)
+                eval = minimax(position, depth - 1, alpha, beta, len(opponents_moves) == 0)
                 maxEval = max(maxEval, eval)
 
+                alpha = max(alpha, eval)
                 position.board[row, col] = Board.EMPTY
-        return maxEval
+                if beta <= alpha:
+                    break
+
+                return maxEval
 
     # else minimizing player's turn
     minEval = float('+inf')
@@ -26,10 +30,14 @@ def minimax(position: Board, depth: int, isMaximizingPlayer: bool) -> int:
             position.board[row, col] = Board.WHITE
 
             opponents_moves = position.all_legal_moves(Board.BLACK)
-            eval = minimax(position, depth - 1, len(opponents_moves) != 0)
+            eval = minimax(position, depth - 1, alpha, beta, len(opponents_moves) != 0)
             minEval = min(minEval, eval)
 
+            beta = min(beta, eval)
             position.board[row, col] = Board.EMPTY
+            if beta <= alpha:
+                break
+
     return minEval
 
 def find_best_move(position: Board) -> tuple[int, int]:
@@ -41,7 +49,7 @@ def find_best_move(position: Board) -> tuple[int, int]:
         if position.board[row, col] == Board.EMPTY:
             position.board[row, col] = Board.WHITE
 
-            currentEval = minimax(position, 5, False)
+            currentEval = minimax(position, 3, float('-inf'), float('inf'), False)
 
             position.board[row, col] = Board.EMPTY
 
